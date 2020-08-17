33fb2c723aad423a88427c1c2c07a4a8b4f139e8
xiaozhanxiang <xiaozhanxiang@qq.com>
2020-08-17 14:35:22 +0800

    modify

--- src/main/java/xzx/starterdemo/config/TestConfig.java
+++ src/main/java/xzx/starterdemo/config/TestConfig.java
@@ -1,5 +1,7 @@
 package xzx.starterdemo.config;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,12 +12,18 @@ import xzx.starterdemo.service.TestService;
  * @author xzx
  * @date 2020/7/21
  */
+@Slf4j
 @Configuration
 @EnableConfigurationProperties(TestMsg.class)
-public class TestConfig {
+public class TestConfig implements InitializingBean {
 
     @Bean
     public TestService testService(TestMsg testMsg) {
         return new TestService(testMsg);
+    }
+
+    @Override
+    public void afterPropertiesSet() {
+        log.info("[InitializingBean] afterPropertiesSet() invoke : TestService has bean Initialized! ");
     }
 }
