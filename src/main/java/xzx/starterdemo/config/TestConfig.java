511d2f1c84bb5042005c113c293349e144b9ac0b
xiaozhanxiang <xiaozhanxiang@qq.com>
2020-08-17 14:53:32 +0800

    modify

--- src/main/java/xzx/starterdemo/config/TestConfig.java
+++ src/main/java/xzx/starterdemo/config/TestConfig.java
@@ -1,7 +1,7 @@
 package xzx.starterdemo.config;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,7 +14,7 @@ import xzx.starterdemo.service.TestService;
  */
 @Slf4j
 @Configuration
-@ConditionalOnClass(EnableTestConfig.class)
+@ConditionalOnBean(EnableTestConfig.class)
 @EnableConfigurationProperties(TestMsg.class)
 public class TestConfig {
 
